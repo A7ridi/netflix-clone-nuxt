23113551df91164d83b9356941814dafa2944c56
a7ridi <a7ridi@github.com>
2021-09-15 17:37:35 +0530

    api added

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,23 @@
 <template>
-  <Tutorial/>
+  <div class="home">
+    <Hero />
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=d0d3b9b8ff1204a42d67cff655e90361&language=en-US&page=1'
+      )
+      const result = await data
+    },
+  },
+}
 </script>
